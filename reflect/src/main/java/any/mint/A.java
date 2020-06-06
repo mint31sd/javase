93bcc22de996d3772b83728bc0d130684163a2b7
mint31sd <mint31@aliyun.com>
2020-06-06 23:46:11 +0800

    reflect

---
+++ reflect/src/main/java/any/mint/A.java
@@ -0,0 +1,11 @@
+package any.mint;
+
+public class A{
+    public void hello() {
+        System.out.println("hello world");
+    }
+
+    public void hello(String say) {
+        System.out.println(say);
+    }
+}
---
+++ reflect/src/main/java/any/mint/TestClassLoad.java
@@ -0,0 +1,17 @@
+package any.mint;
+
+import com.sun.deploy.util.StringUtils;
+
+import java.lang.reflect.Method;
+
+public class TestClassLoad {
+    public static void main(String[] args) throws Exception {
+        Class<?> clz = Class.forName("any.mint.A");
+        Object o = clz.newInstance();
+        Method m = clz.getDeclaredMethod("hello", null);
+        m.invoke(o);
+
+        Method m2 = clz.getMethod("hello", String.class);
+        m2.invoke(o,"你好!");
+    }
+}
