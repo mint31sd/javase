0a1891d3ddf9ae4ff783f360f8a2972fbdcbc658
mint31sd <mint31@aliyun.com>
2020-06-07 23:58:30 +0800

    代码格式

--- reflect/src/main/java/any/mint/annotation/User.java
+++ reflect/src/main/java/any/mint/annotation/User.java
@@ -6,15 +6,15 @@ import lombok.Setter;
 /**
  * 定义数据模型
  * 这里用User类来表示具体待处理的数据对象
- *
  */
 @Setter
 @Getter
 public class User {
     private String username;
+
     @ValidateAge(min = 20, max = 35, value = 22)
     private int age;
+
     @InitSex(sex = InitSex.SEX_TYPE.MAN)
     private String sex;
-    // 省略getter/setter方法
 }
